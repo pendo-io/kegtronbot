5e4ee5624a158fe8d443137c158ebf6541b44743
Josh Connelly <josh.connelly@pendo.io>
2020-12-01 10:07:33 -0500

    switch to node

--- deploy-prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-go mod vendor
-go build -mod=vendor ./...
-gcloud --project pendo-kegtronbot app deploy internal/app/app.yaml --quiet --promote --no-stop-previous-version
--- deploy-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-go mod vendor
-go build -mod=vendor ./...
-gcloud --project pendo-kegtronbot-test app deploy internal/app/app.yaml --quiet --promote --no-stop-previous-version
